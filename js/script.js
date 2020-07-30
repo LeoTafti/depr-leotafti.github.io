41da0d6a82d15d4484f43ea2c8d3a431b554093c
Léo Tafti <leotafti@gmail.com>
2020-07-30 16:46:13 +0200

    Added "About me" section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
-function animScroll(){
+/*Only show the "About me" section when at the top of the page*/
+var visibleBottom = $('.visible-top-only').position().top + $('.visible-top-only').outerHeight(false);
+/*NOT robust (needs fix): fails if window is resized when the "About me" section isn't visible*/
+$(window).resize(function() {
+    visibleBottom = $('.visible-top-only').position().top + $('.visible-top-only').outerHeight(false);
+});
+
+function visibleTopOnlyScroll() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    // if ($(this).scrollTop() > 100) {
+    if (windowBottom > visibleBottom + 150 && $(this).scrollTop() > 100) {
+      $('.visible-top-only').fadeOut(function(){
+          $('.content').fadeIn();
+      })
+    } else {
+      $('.content').fadeOut(function(){
+          $('.visible-top-only').fadeIn();
+      });
+    }
+}
+
+function fadeOnScroll(){
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
     $(".fade-on-scroll").each(function() {
         /* Check the location of each desired element */
@@ -13,7 +34,12 @@ function animScroll(){
     });
 }
 
-/*Otherwise sometimes too many scroll events make animation fail*/
+function animScroll(){
+    fadeOnScroll();
+    visibleTopOnlyScroll();
+}
+
+/*Otherwise sometimes too many scroll events make the animation fail*/
 var animScrollThrotteled = _.throttle(animScroll, 100);
 
 $(window).on("load",function() {
