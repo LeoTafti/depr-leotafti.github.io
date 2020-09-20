9815561aac01a58c0744625dd67165bb04f41033
Léo Tafti <leotafti@gmail.com>
2020-09-20 21:53:25 +0200

    Website redisign

--- js/script.js
+++ js/script.js
@@ -1,53 +1,28 @@
-/*Only show the "About me" section when at the top of the page*/
-var visibleBottom = $('.visible-top-only').position().top + $('.visible-top-only').outerHeight(false);
-/*NOT robust (needs fix): fails if window is resized when the "About me" section isn't visible*/
-$(window).resize(function() {
-    visibleBottom = $('.visible-top-only').position().top + $('.visible-top-only').outerHeight(false);
-});
-
-function visibleTopOnlyScroll() {
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    // if ($(this).scrollTop() > 100) {
-    if (windowBottom > visibleBottom + 150 && $(this).scrollTop() > 100) {
-      $('.visible-top-only').fadeOut(function(){
-          $('.content').fadeIn();
-      })
-    } else {
-      $('.content').fadeOut(function(){
-          $('.visible-top-only').fadeIn();
-      });
+function navbarClickedOn(divString){
+    switch (divString) {
+        case 'about':
+            showDiv(document.getElementById('bio'));
+            break;
+        case 'education':
+            showDiv(document.getElementById('education'));
+            break;
+        case 'experience':
+            showDiv(document.getElementById('experience'));
+            break;
+        case 'projects':
+            showDiv(document.getElementById('projects'));
+            break;
+        default:
+            break;
     }
 }
 
-function fadeOnScroll(){
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $(".fade-on-scroll").each(function() {
-        /* Check the location of each desired element */
-        var objectTop = $(this).offset().top;
-        
-        /* If the element is completely within bounds of the window, fade it in */
-        if (objectTop + 120 < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(1000,1);}
-        } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(1000,0);}
-        }
-    });
+shownDiv = document.getElementById('bio')
+
+function showDiv(div){
+    $(shownDiv).fadeOut(300, function(){
+        shownDiv.style.display='none'
+        shownDiv = div;
+        $(div).fadeIn(300);
+    })
 }
-
-function animScroll(){
-    fadeOnScroll();
-    visibleTopOnlyScroll();
-}
-
-/*Otherwise sometimes too many scroll events make the animation fail*/
-var animScrollThrotteled = _.throttle(animScroll, 100);
-
-$(window).on("load",function() {
-    $(window).scroll(animScrollThrotteled).scroll(); //invoke scroll-handler on page-load
-  });
-
-$(document).ready(function() {
-    $('.fade-on-load').each( function(i){
-        $(this).animate({'opacity':'1'},2000);
-    });
-});
